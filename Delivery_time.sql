30b00d68a678274599d825d1df7d6422151f86c6
Rushik Chorapalli <rushik119.ch@gmail.com>
2025-02-27 11:47:27 +0530

    Delete Delivery_time.sql

--- Delivery_time.sql
+++
@@ -1,4 +0,0 @@
-SELECT agent_name,location,MAX(delivery_time) AS Slowest_delivery,MIN(delivery_time) AS Fastest_delivery
-from delivery_agent_reviews
-group by agent_name,location
-order by location ASC;
